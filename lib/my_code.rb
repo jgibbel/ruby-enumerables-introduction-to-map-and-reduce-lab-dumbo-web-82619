9a2045257f8e38e1b417a4601272634427596432
James Gibbel <jamesgibbel@gmail.com>
2019-08-06 07:25:09 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -47,7 +47,8 @@ def reduce_to_total(source_array, starting_point=0)
   while (i < source_array.length) do 
     total += source_array[i]
     i+=1 
-  end 
+  end
+  i +=starting_point
   return total
 end
 
